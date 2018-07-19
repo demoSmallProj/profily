3414f1a44ff1f2cc7a4bfe3edf2537ed6b75cc6f
demoSmallProj <demoSmallProj@gmail.com>
2018-07-19 21:17:44 +1000

    add spring boot scaffolds by initializr

---
+++ profily-core/src/main/java/org/mytask/profilycore/ProfilyCoreApplication.java
@@ -0,0 +1,12 @@
+package org.mytask.profilycore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfilyCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfilyCoreApplication.class, args);
+	}
+}
---
+++ profily-core/src/test/java/org/mytask/profilycore/ProfilyCoreApplicationTests.java
@@ -0,0 +1,16 @@
+package org.mytask.profilycore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProfilyCoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
