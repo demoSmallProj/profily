23d29a54b98eea71b709a09c15e5ab41240a53c1
demoSmallProj <demoSmallProj@gmail.com>
2018-07-20 01:40:58 +1000

    add cors

--- profily-core/src/main/java/org/mytask/profilycore/controller/CustomerController.java
+++ profily-core/src/main/java/org/mytask/profilycore/controller/CustomerController.java
@@ -2,12 +2,11 @@ package org.mytask.profilycore.controller;
 
 import org.mytask.profilycore.resource.Customer;
 import org.mytask.profilycore.service.PaymentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -15,30 +14,20 @@ import java.util.Date;
 
 @RestController
 public class CustomerController {
+    public static final Logger logger = LoggerFactory.getLogger(CustomerController.class);
 
     @Autowired
     PaymentService paymentService;
 
     @GetMapping("/customer/{id}")
+    @CrossOrigin(origins = "http://localhost:3000")
     public Customer getCustomer(@PathVariable("id") long customerId,
                                 @DateTimeFormat(pattern = "yyyy-MM-dd")
                                 @RequestParam("month") Date month,
                                 @DateTimeFormat(pattern = "yyyy-MM-ddThh:mm:ss")
                                 @RequestParam(value = "balanceTime", required = false) Date balanceTime) {
+        logger.info("request received for customerId: {}, month: {}, balanceTime: {}", customerId, month, balanceTime);
         Customer customer = paymentService.getCustomer(customerId, month, balanceTime);
-        return customer;
-    }
-
-    //TODO: drop later
-    @GetMapping("/tmp")
-    public Customer tmp() {
-        Date month = null;
-        try {
-            month = new SimpleDateFormat("dd/MM/yyyy").parse("29/05/2016");
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        Customer customer = paymentService.getCustomer(3, month, month);
         return customer;
     }
 }
